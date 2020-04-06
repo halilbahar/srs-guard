e4b41d421c3cabb078879aa997eaa3009cfc2afb
halilbahar <halil.s.bahar@gmail.com>
2020-04-06 18:14:30 +0200

    Create Role entity

--- src/main/java/at/htl/srsguard/entity/Role.java
+++ src/main/java/at/htl/srsguard/entity/Role.java
@@ -1,26 +1,25 @@
 package at.htl.srsguard.entity;
 
-import javax.persistence.*;
-import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
-public class Account {
+public class Role {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private String key;
     private String description;
-    @ManyToMany
-    private List<Role> roles;
 
-    public Account(String name, String description) {
+    public Role(String name, String description) {
         this.name = name;
         this.description = description;
     }
 
-    public Account() {
+    public Role() {
     }
 
     public Long getId() {
@@ -37,14 +36,6 @@ public class Account {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getKey() {
-        return key;
-    }
-
-    public void setKey(String key) {
-        this.key = key;
     }
 
     public String getDescription() {
