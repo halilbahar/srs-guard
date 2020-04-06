ea79489eacae493f810730758991047f19b97243
halilbahar <halil.s.bahar@gmail.com>
2020-04-06 20:23:05 +0200

    Create method for persisting

--- src/main/java/at/htl/repository/AccountRepository.java
+++ src/main/java/at/htl/repository/AccountRepository.java
@@ -4,7 +4,13 @@ import at.htl.srsguard.entity.Account;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
 
 @ApplicationScoped
 public class AccountRepository implements PanacheRepository<Account> {
+
+    @Transactional
+    public void persistAccount(Account account) {
+        this.persist(account);
+    }
 }
