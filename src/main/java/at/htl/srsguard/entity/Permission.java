bd5cec92fa9a2d047fce871104316f7c76381674
halilbahar <halil.s.bahar@gmail.com>
2020-04-06 18:23:33 +0200

    Generate code for Permission

--- src/main/java/at/htl/srsguard/entity/Permission.java
+++ src/main/java/at/htl/srsguard/entity/Permission.java
@@ -10,4 +10,38 @@ public class Permission {
     private Long id;
     @ManyToOne
     private App app;
+    @ManyToOne
+    private Stream stream;
+
+    public Permission(App app, Stream stream) {
+        this.app = app;
+        this.stream = stream;
+    }
+
+    public Permission() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public App getApp() {
+        return app;
+    }
+
+    public void setApp(App app) {
+        this.app = app;
+    }
+
+    public Stream getStream() {
+        return stream;
+    }
+
+    public void setStream(Stream stream) {
+        this.stream = stream;
+    }
 }
