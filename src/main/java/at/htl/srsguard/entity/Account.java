759ec572ef1129e90d463239cb2ae350a61a80ca
halilbahar <halil.s.bahar@gmail.com>
2020-04-06 18:11:56 +0200

    Create Account entity

---
+++ src/main/java/at/htl/srsguard/entity/Account.java
@@ -0,0 +1,57 @@
+package at.htl.srsguard.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String key;
+    private String description;
+
+    public Account(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Account() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
