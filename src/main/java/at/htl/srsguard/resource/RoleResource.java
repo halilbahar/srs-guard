7ff0a9afbaf9b0ebdf344c46a54803c05ff8e2d2
halilbahar <halil.s.bahar@gmail.com>
2020-04-07 12:48:39 +0200

    Get role by id from endpoint

--- src/main/java/at/htl/srsguard/resource/RoleResource.java
+++ src/main/java/at/htl/srsguard/resource/RoleResource.java
@@ -1,10 +1,12 @@
 package at.htl.srsguard.resource;
 
+import at.htl.srsguard.entity.Role;
 import at.htl.srsguard.repository.RoleRepository;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
@@ -18,5 +20,17 @@ public class RoleResource {
     @Produces("application/json")
     public Response getAllRoles() {
         return Response.ok(this.roleRepository.findAll().list()).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces("application/json")
+    public Response getRole(@PathParam("id") Long id) {
+        Role role = this.roleRepository.find("id", id).firstResult();
+        if (role == null) {
+            return Response.status(404).build();
+        }
+
+        return Response.ok(role).build();
     }
 }
