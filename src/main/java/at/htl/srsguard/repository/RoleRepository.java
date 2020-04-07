91a4ddd5afdc5f0c24af30acade5f6248f149c8f
halilbahar <halil.s.bahar@gmail.com>
2020-04-07 12:45:41 +0200

    Make RoleRepository to cdi bean

--- src/main/java/at/htl/srsguard/repository/RoleRepository.java
+++ src/main/java/at/htl/srsguard/repository/RoleRepository.java
@@ -3,8 +3,10 @@ package at.htl.srsguard.repository;
 import at.htl.srsguard.entity.Role;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.transaction.Transactional;
 
+@ApplicationScoped
 public class RoleRepository implements PanacheRepository<Role> {
 
     @Transactional
