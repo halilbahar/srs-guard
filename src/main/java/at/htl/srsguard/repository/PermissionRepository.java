e61305281746d574d3a9becda5aaefe501b729a2
halilbahar <halil.s.bahar@gmail.com>
2020-04-08 10:52:13 +0200

    Create repository for Permission

---
+++ src/main/java/at/htl/srsguard/repository/PermissionRepository.java
@@ -0,0 +1,21 @@
+package at.htl.srsguard.repository;
+
+import at.htl.srsguard.entity.Permission;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
+
+@ApplicationScoped
+public class PermissionRepository implements PanacheRepository<Permission> {
+
+    @Transactional
+    public void persistPermission(Permission permission) {
+        this.persist(permission);
+    }
+
+    @Transactional
+    public boolean deletePermission(Long id) {
+        return this.delete("id", id) != 0;
+    }
+}
