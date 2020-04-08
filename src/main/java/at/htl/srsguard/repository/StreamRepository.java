7224443370ed95a1fb6ed10dcfc8d95671d29f37
halilbahar <halil.s.bahar@gmail.com>
2020-04-08 10:53:22 +0200

    Create repository for Stream

---
+++ src/main/java/at/htl/srsguard/repository/StreamRepository.java
@@ -0,0 +1,21 @@
+package at.htl.srsguard.repository;
+
+import at.htl.srsguard.entity.Stream;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
+
+@ApplicationScoped
+public class StreamRepository implements PanacheRepository<Stream> {
+
+    @Transactional
+    public void persistStream(Stream stream) {
+        this.persist(stream);
+    }
+
+    @Transactional
+    public boolean deleteStream(Long id) {
+        return this.delete("id", id) != 0;
+    }
+}
