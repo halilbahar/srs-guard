ec612141b3d1f80244570548b0585fda391c78c0
halilbahar <halil.s.bahar@gmail.com>
2020-04-30 16:49:17 +0200

    Create Role Test

---
+++ src/test/java/at/htl/resource/RoleResourceTest.java
@@ -0,0 +1,61 @@
+package at.htl.resource;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import javax.json.JsonObject;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.any;
+
+@QuarkusTest
+public class RoleResourceTest {
+
+    @Test
+    public void testGetAllRoles() {
+        given()
+        .when()
+            .get("/role")
+        .then()
+            .statusCode(200)
+            .contentType(JSON)
+            .body("size()", is(0));
+    }
+
+    ////////////////////
+    // Util functions //
+    ////////////////////
+
+    private Integer createRole(JsonObject payload) {
+        Number id = given()
+            .contentType(JSON)
+            .body(payload.toString())
+        .when()
+            .post("/role")
+        .then()
+            .statusCode(200)
+            .contentType(JSON)
+            .body("name", is(payload.getString("name")))
+            .body("description", is(payload.getString("description")))
+            .body("id", isA(Number.class))
+        .extract()
+            .path("id");
+
+        return id.intValue();
+    }
+
+    private void deleteRole(Integer id) {
+        given()
+                .pathParam("id", id)
+                .when()
+                .delete("/account/{id}")
+                .then()
+                .statusCode(200)
+                .contentType(JSON)
+                .body("name", is(any(String.class)))
+                .body("description", is(any(String.class)))
+                .body("id", is(id));
+    }
+}
