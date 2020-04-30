fbdfdb047f7c7f0d919318fa0e9efb14471c1ada
halilbahar <halil.s.bahar@gmail.com>
2020-04-30 18:32:06 +0200

    Change permission path

--- src/main/java/at/htl/srsguard/resource/RoleResource.java
+++ src/main/java/at/htl/srsguard/resource/RoleResource.java
@@ -73,7 +73,7 @@ public class RoleResource {
     }
 
     @POST
-    @Path("/permission/{id}")
+    @Path("/{id}/permission")
     @Consumes("application/json")
     @Produces("application/json")
     @Transactional
@@ -98,7 +98,7 @@ public class RoleResource {
     }
 
     @DELETE
-    @Path("/permission/{id}")
+    @Path("/{id}/permission")
     @Consumes("application/json")
     @Transactional
     public Response removePermissions(@PathParam("id") Long id, List<AppStream> appStreamList) {
